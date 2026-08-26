43737e72dab8f7b29e7712c59be4e34ddf240f81
Alberto <asanchez@usfq.edu.ec>
2026-08-26 17:34:24 -0500

    new tcl

--- hardware/scripts/build.tcl
+++ hardware/scripts/build.tcl
@@ -1,8 +1,20 @@
 # build.tcl
+# 1. Force local BD creation (overrides remote path checks in generated BD scripts)
+set run_remote_bd_flow 0
+
+# 2. Build base project and add custom sources
 source ./scripts/baseSystem.tcl
-add_files -norecurse [glob ./src/*.sv ./src/*.v]
-update_compile_order -fileset sources_1
+
+if {[llength [glob -nocomplain ./src/*.sv ./src/*.v]] > 0} {
+    add_files -norecurse [glob ./src/*.sv ./src/*.v]
+    update_compile_order -fileset sources_1
+}
+
+# 3. Generate Block Design
 source ./scripts/system_bd.tcl
-make_wrapper -files [get_files *.bd] -top
-add_files -norecurse [glob ./*.gen/sources_1/bd/*/hdl/*_wrapper.*]
+
+# 4. Generate and add top-level wrapper directly (eliminates .gen glob failure)
+set bd_file [get_files *.bd]
+set wrapper_file [make_wrapper -files $bd_file -top]
+add_files -norecurse $wrapper_file
 update_compile_order -fileset sources_1
