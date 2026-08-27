dbce3980f5d9a7d7e9e2d7697ed10226f70f4bd2
Alberto <asanchez@usfq.edu.ec>
2026-08-27 10:49:30 -0500

    simplified tcl

--- hardware/scripts/build.tcl
+++ hardware/scripts/build.tcl
@@ -1,17 +1,2 @@
 # build.tcl
-
-# 1. Source base project setup
 source ./scripts/baseSystem.tcl
-
-# 2. Close and remove any existing Block Design in memory/project files
-#if {[current_bd_design -quiet] ne ""} {
-#    close_bd_design [current_bd_design]
-#}
-#set old_bd [get_files -quiet *.bd]
-#if {$old_bd ne ""} {
-#    remove_files $old_bd
-#}
-
-# 3. Generate Block Design from Tcl
-#source ./scripts/system_bd.tcl
-
